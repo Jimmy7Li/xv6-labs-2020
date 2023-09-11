72e3ceb8db1f165c0537c619c686cc0435facbf4
Jimmy7Li <1455373009@qq.com>
2023-09-11 17:55:43 +0800

    只做了trace系统调用

--- kernel/proc.c
+++ kernel/proc.c
@@ -150,6 +150,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask=0;
 }
 
 // Create a user page table for a given process,
@@ -291,6 +292,7 @@ fork(void)
 
   safestrcpy(np->name, p->name, sizeof(p->name));
 
+  np->trace_mask=p->trace_mask;
   pid = np->pid;
 
   np->state = RUNNABLE;
